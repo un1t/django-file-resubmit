350e1a892e82d180ce0a74ed19dd9d419e9ead3a
Ilya Shalyapin <ishalyapin@gmail.com>
2011-07-29 23:34:58 +0600

    Dirty FileField support.

--- file_resubmit/admin.py
+++ file_resubmit/admin.py
@@ -6,6 +6,8 @@ import time
  
 from django import forms
 from django.conf import settings
+from django.db import models
+from django.forms import ClearableFileInput
 from django.utils.safestring import mark_safe
 
 from sorl.thumbnail.fields import ImageField
@@ -22,16 +24,32 @@ def random_key():
 
 class AdminResubmitWidget(AdminImageWidget):
     
-    def __init__(self, attrs=None):
+    def __init__(self, attrs=None, field_type=None):
         super(AdminResubmitWidget, self).__init__()
         self.cache_key = '' 
+        self.field_type = field_type
     
     def render(self, name, value, attrs=None):
-        output = super(AdminResubmitWidget, self).render(name, value, attrs)
+        if value and hasattr(value, 'url') and self.field_type == models.FileField:
+            self.template_with_initial = ClearableFileInput.template_with_initial
+            self.template_with_clear = ClearableFileInput.template_with_clear
+            output = ClearableFileInput.render(self, name, value, attrs)
+            
+        else:
+            output = super(AdminResubmitWidget, self).render(name, value, attrs)
+            if value and self.cache_key:
+                output += ' ' + self.filename_from_value(value)
+            
         if self.cache_key:
             output += forms.HiddenInput().render(self.input_name, self.cache_key, {})
-            if value:
-                output += ' ' + os.path.split(value.name)[-1]
+                
+                
+#        if value and hasattr(value, 'url'):
+#            if self.field_type == models.FileField:
+#                output += (
+#                        u'<a style="display:block;margin:0 0 10px" target="_blank" href="%s">%s</a>'
+#                    ) % (value.url, self.filename_from_value(value)) 
+        
         return mark_safe(output)
 
     def value_from_datadict(self, data, files, name):
@@ -53,6 +71,10 @@ class AdminResubmitWidget(AdminImageWidget):
                 upload.file.seek(0)
         return upload
     
+    def filename_from_value(self, value):
+        if value: 
+            return os.path.split(value.name)[-1]
+    
     
 class AdminResubmitMixin(object):
     """
@@ -61,6 +83,8 @@ class AdminResubmitMixin(object):
     """
     def formfield_for_dbfield(self, db_field, **kwargs):
         if isinstance(db_field, ImageField):
-            return db_field.formfield(widget=AdminResubmitWidget)
+            return db_field.formfield(widget=AdminResubmitWidget(field_type=ImageField))
+        elif isinstance(db_field, models.FileField):
+            return db_field.formfield(widget=AdminResubmitWidget(field_type=models.FileField))
         sup = super(AdminResubmitMixin, self)
         return sup.formfield_for_dbfield(db_field, **kwargs)
