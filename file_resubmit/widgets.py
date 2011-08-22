1aee10e7a1ec0ba20b3a4927942351f4199222a8
MakeToLearn <jordan@aace.org>
2011-08-22 16:55:50 -0400

    Just simple widgets that don't need admin or sorl

---
+++ file_resubmit/widgets.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import os
+import hashlib  
+import random
+import time
+ 
+from django import forms
+from django.forms.widgets import FILE_INPUT_CONTRADICTION
+from django.conf import settings
+from django.forms import ClearableFileInput
+from django.utils.safestring import mark_safe
+
+from file_resubmit.models import FileCache
+
+
+class ResubmitBaseWidget(forms.widgets.FileInput):
+    
+    def __init__(self, attrs=None, field_type=None):
+        super(ResubmitBaseWidget, self).__init__()
+        self.cache_key = '' 
+        self.field_type = field_type
+
+    def value_from_datadict(self, data, files, name):
+        upload = super(ResubmitBaseWidget, self).value_from_datadict(data, files, name)
+        if upload == FILE_INPUT_CONTRADICTION:
+            return upload
+        
+        self.input_name = "%s_cache_key" % name
+        self.cache_key = data.get(self.input_name, "")
+        
+        if files.has_key(name):
+            self.cache_key = self.random_key()[:10]
+            upload = files[name]
+            FileCache().set(self.cache_key, upload)
+        elif self.cache_key:
+            restored = FileCache().get(self.cache_key, name)
+            if restored:
+                upload = restored
+                files[name] = upload
+        return upload
+    
+    def random_key(self):
+        x = "%s%s%s" % (settings.SECRET_KEY, time.time(), random.random())
+        random.seed(x)
+        return hashlib.md5(str(random.random())).hexdigest()
+    
+    def output_extra_data(self, value):
+        output = ''
+        if value and self.cache_key:
+            output += ' ' + self.filename_from_value(value)
+        if self.cache_key:
+            output += forms.HiddenInput().render(self.input_name, self.cache_key, {})
+        return output
+    
+    def filename_from_value(self, value):
+        if value: 
+            return os.path.split(value.name)[-1]
+        
+    
+class ResubmitFileWidget(ResubmitBaseWidget):
+    template_with_initial = ClearableFileInput.template_with_initial
+    template_with_clear = ClearableFileInput.template_with_clear
+    
+    def render(self, name, value, attrs=None):
+        output = ClearableFileInput.render(self, name, value, attrs)
+        output += self.output_extra_data(value)
+        return mark_safe(output)
+
+class ResubmitImageWidget(ResubmitBaseWidget):
+    def render(self, name, value, attrs=None):
+        output = super(ResubmitImageWidget, self).render(name, value, attrs)
+        output += self.output_extra_data(value)
+        return mark_safe(output)
